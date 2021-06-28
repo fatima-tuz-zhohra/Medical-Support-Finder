7945cf6338a2a81a5f95b0c98c7c6a726680f55b
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-06-28 19:58:34 +0600

    loginpage

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -12,87 +12,100 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFEFEFA),
-      appBar: AppBar(
-          //title: Text(widget.title),
-          title: Row(
-        children: [
-          Icon(
-            Icons.star,
-            color: Colors.black,
-            size: 20.0,
-          ),
-          const SizedBox(
-            width: 30,
-          ),
-          Text('MSF'),
-        ],
-      )),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    filled: true,
-                    //fillColor: Colors.blueAccent,
-                    border: OutlineInputBorder(),
-                    labelText: 'Email')),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: TextField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    filled: true,
-                    //fillColor: Colors.blueAccent,
-                    border: OutlineInputBorder(),
-                    labelText: 'Password')),
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.all(16.0),
-              textStyle: const TextStyle(fontSize: 20),
+        backgroundColor: Color(0xFFFEFEFA),
+        appBar: AppBar(
+            //title: Text(widget.title),
+            title: Row(
+          children: [
+            Icon(
+              Icons.star,
+              color: Colors.black,
+              size: 20.0,
             ),
-            onPressed: () {},
-            child: const Text('LogIn'),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            const SizedBox(
+              width: 30,
+            ),
+            Text('MSF'),
+          ],
+        )),
+        body: SingleChildScrollView(
+          child: Column(
             children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(16.0),
-                  textStyle: const TextStyle(fontSize: 20),
-                ),
-                onPressed: () {},
-                child: const Text('Google'),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        filled: true,
+                        //fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(),
+                        labelText: 'Email')),
               ),
               const SizedBox(
-                width: 30,
+                height: 4,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        filled: true,
+                        //fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(),
+                        labelText: 'Password')),
+              ),
+              const SizedBox(
+                height: 4,
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(12.0),
                   textStyle: const TextStyle(fontSize: 20),
                 ),
                 onPressed: () {},
-                child: const Text('Facebook'),
+                child: const Text('LogIn'),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(12.0),
+                      //textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Google'),
+                  ),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(12.0),
+                      //textStyle: const TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Facebook'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Do not have an Account?'),
+                  TextButton(
+                      onPressed:(){
+
+                  },
+                    child: Text('SignUp'),
+                  )
+                ],
               ),
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
