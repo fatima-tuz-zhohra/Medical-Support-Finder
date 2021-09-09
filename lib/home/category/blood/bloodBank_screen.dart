22c17e7a2f89373c01058dcdf2a0f5e903ca20b0
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-09-09 20:48:16 +0600

    Move bloodbank list to separate class

--- lib/home/category/blood/bloodBank_screen.dart
+++ lib/home/category/blood/bloodBank_screen.dart
@@ -15,91 +15,66 @@ class BloodBankScreen extends StatefulWidget {
 class _BloodBankScreenState extends State<BloodBankScreen> {
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('Blood Bank'),
       ),
-      body: Column(
-        children: [
-          SearchView(onTextChange: (String ) { },
-          ),
-          
-          Expanded(
-            child: StreamBuilder<QuerySnapshot<Object?>>(
-              stream: BloodBankService().getBloodBank(),
-                builder: (context, snapshot){
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  }
-                  else if (snapshot.hasData) {
-                    final data = snapshot.requireData;
-                    final List<BloodBankItem> bloodBanks = [];
+      body: StreamBuilder<QuerySnapshot<Object?>>(
+          stream: BloodBankService().getBloodBank(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              final data = snapshot.requireData;
+              final List<BloodBankItem> bloodBanks = [];
 
-                    data.docs.forEach((element) {
-                      final dbItem = element.data()! as Map<String, dynamic>;
-                      final bloodBank = BloodBankItem(
-                        dbItem['name'],
-                        dbItem['address'],
-                        dbItem['phoneNo'],
-                        double.parse("${dbItem['latitude']}"),
-                        double.parse("${dbItem['longitude']}"),
-                      );
+              data.docs.forEach((element) {
+                final dbItem = element.data()! as Map<String, dynamic>;
+                final bloodBank = BloodBankItem(
+                  dbItem['name'],
+                  dbItem['address'],
+                  dbItem['phoneNo'],
+                  double.parse("${dbItem['latitude']}"),
+                  double.parse("${dbItem['longitude']}"),
+                );
 
-                      bloodBanks.add(bloodBank);
-                    });
-                    return ListView.builder(
-                        itemCount: bloodBanks.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            child: ListTile(
-                              title: Text('${bloodBanks[index].name}'),
-                              subtitle: Text('${bloodBanks[index].address}'),
-                              trailing: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    iconSize: 18,
-                                    icon: const Icon(Icons.location_on),
-                                    color: theme.colorScheme.secondary,
-                                    onPressed: () {
-                                      navigateTo(bloodBanks[index].latitude,
-                                          bloodBanks[index].longitude);
-                                    },
-                                  ),
-                                  IconButton(
-                                    iconSize: 18,
-                                    icon: const Icon(Icons.call),
-                                    color: theme.colorScheme.secondary,
-                                    onPressed: () {
-                                      launch(('tel://${bloodBanks[index]
-                                          .phoneNo}'));
-                                    },
-                                  )
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-                    );
-                  }
-                  else {
-                    return Container();
-                  }
-                }
-            ),
+                bloodBanks.add(bloodBank);
+              });
+              return _BloodListContent(bloodBanks: bloodBanks);
+            } else {
+              return Container();
+            }
+          }),
+    );
+  }
+}
 
-            /*ListView.builder(
-              itemCount: bloodBankEntries.length,
-              itemBuilder: (context, index) {
+class _BloodListContent extends StatelessWidget {
+  const _BloodListContent({Key? key, required this.bloodBanks})
+      : super(key: key);
+  final List<BloodBankItem> bloodBanks;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Column(
+      children: [
+        SearchView(onTextChange: (text) {
+          print(text);
+        }),
+        Expanded(
+          child: ListView.builder(
+              itemCount: bloodBanks.length,
+              itemBuilder: (BuildContext context, int index) {
                 return Card(
                   child: ListTile(
-                    title: Text('${bloodBankEntries[index].name}'),
-                    subtitle: Text('${bloodBankEntries[index].address}'),
-                    trailing:
-                    Row(
+                    title: Text('${bloodBanks[index].name}'),
+                    subtitle: Text('${bloodBanks[index].address}'),
+                    trailing: Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       mainAxisSize: MainAxisSize.min,
                       children: [
@@ -107,32 +82,29 @@ class _BloodBankScreenState extends State<BloodBankScreen> {
                           iconSize: 18,
                           icon: const Icon(Icons.location_on),
                           color: theme.colorScheme.secondary,
-                          onPressed:() {
-                            navigateTo(bloodBankEntries[index].latitude, bloodBankEntries[index].longitude);
+                          onPressed: () {
+                            navigateTo(bloodBanks[index].latitude,
+                                bloodBanks[index].longitude);
                           },
                         ),
                         IconButton(
                           iconSize: 18,
                           icon: const Icon(Icons.call),
                           color: theme.colorScheme.secondary,
-                          onPressed:() {
-                            launch(('tel://${bloodBankEntries[index].phoneNo}'));
+                          onPressed: () {
+                            launch(('tel://${bloodBanks[index].phoneNo}'));
                           },
                         )
                       ],
                     ),
-
-
                   ),
                 );
-              },
-            ),*/
-
-          ),
-        ],
-      ),
-      );
+              }),
+        ),
+      ],
+    );
   }
+
   void navigateTo(double lat, double lng) async {
     var uri = Uri.parse("http://maps.google.com/maps?daddr=$lat,$lng");
     if (await canLaunch(uri.toString())) {
