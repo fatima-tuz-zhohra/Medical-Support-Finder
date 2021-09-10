a04596e912dd2b4ce9cb9457d8e0373b4bc18e28
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-09-10 21:45:40 +0600

    adding settings features

--- lib/home/home_main/settings/setting.dart
+++ lib/home/home_main/settings/setting.dart
@@ -11,14 +11,48 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Setting Screen"),
-              ],
-            )),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListTile(
+              leading: Icon(Icons.add_alert),
+              title: Text('Receive Notification'),
+              trailing: Switch(
+                value: true,
+                onChanged: (bool value) {},
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text('Help & Support'),
+              trailing: IconButton(
+                icon: const Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.privacy_tip_outlined),
+              title: Text('Privacy Policy'),
+              trailing: IconButton(
+                icon: const Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.content_paste_outlined),
+              title: Text('Terms & Conditions'),
+              trailing: IconButton(
+                icon: const Icon(Icons.arrow_forward_ios),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
