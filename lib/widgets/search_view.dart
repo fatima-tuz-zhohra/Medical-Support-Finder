3ae2170b4601e2c124325740e6c8621e134a0e16
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-09-19 21:58:26 +0600

    Update searchview padding

--- lib/widgets/search_view.dart
+++ lib/widgets/search_view.dart
@@ -8,60 +8,57 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 64,
-            width: size.width * 0.9,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 8, bottom: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.secondary,
-                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: TextFormField(
-                          onChanged: onTextChange,
-                          style: TextStyle(
-                            fontFamily: 'WorkSans',
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                          //keyboardType: TextInputType.text,
-                          decoration: InputDecoration(
-                            filled: true,
-                            labelText: 'For Search ',
-                            border: InputBorder.none,
-                            labelStyle: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16,
-                              letterSpacing: 0.2,
-                            ),
-                          ),
-                          //onEditingComplete: () {},
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 64,
+          width: size.width * 0.9,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8, bottom: 8),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: TextFormField(
+                        onChanged: onTextChange,
+                        style: TextStyle(
+                          fontFamily: 'WorkSans',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
                         ),
+                        //keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          filled: true,
+                          labelText: 'For Search ',
+                          border: InputBorder.none,
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            letterSpacing: 0.2,
+                          ),
+                        ),
+                        //onEditingComplete: () {},
                       ),
                     ),
-                    SizedBox(
-                      width: 50,
-                      height: 60,
-                      child: Icon(Icons.search),
-                    )
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    width: 50,
+                    height: 60,
+                    child: Icon(Icons.search),
+                  )
+                ],
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
