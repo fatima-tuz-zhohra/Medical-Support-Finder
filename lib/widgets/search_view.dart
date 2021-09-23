ff4065d36b23fb9630d3c946dc1d0ca5f293cf8c
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-09-23 23:11:33 +0600

    Refactor search view UI

--- lib/widgets/search_view.dart
+++ lib/widgets/search_view.dart
@@ -8,56 +8,21 @@ class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 64,
-          width: size.width * 0.9,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 8, bottom: 8),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.secondary,
-                borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      child: TextFormField(
-                        onChanged: onTextChange,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                        //keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          filled: true,
-                          labelText: 'For Search ',
-                          border: InputBorder.none,
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16,
-                            letterSpacing: 0.2,
-                          ),
-                        ),
-                        //onEditingComplete: () {},
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 50,
-                    height: 60,
-                    child: Icon(Icons.search),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ],
+    return Container(
+      padding: const EdgeInsets.only(top: 8, bottom: 8),
+      height: 64,
+      width: size.width * 0.9,
+      child: TextFormField(
+        onChanged: onTextChange,
+        keyboardType: TextInputType.text,
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            filled: true,
+            hintText: 'Search...',
+            border: InputBorder.none),
+        //onEditingComplete: () {},
+      ),
     );
   }
 }
